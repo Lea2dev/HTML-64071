9b983a958da280f8a88a14f9e3a201e10e0c81a1
Leandro <leandro.pais@educacionit.com>
2023-07-13 22:19:25 -0300

    php - FIN DE CURSO

---
+++ InterfazWeb/php/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo ("<h1>Hola</h1>");
+
+$nombre = "<h2>Leandro</h2>";
+
+echo ("<h2>Leandro<h2>");
+echo ($nombre);
+
+$apellido = ["Pais", "Touceda"]; //array
+//              0        1
+
+echo($apellido[0]);
+
+?>
